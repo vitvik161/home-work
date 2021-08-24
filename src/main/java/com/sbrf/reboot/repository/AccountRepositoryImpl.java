fe67bdf022c55803e70ac902e86955111f4b07e1
vitvik161 <88671741+vitvik161@users.noreply.github.com>
2021-08-24 20:28:13 +0300

    второй коммит 4 урока

--- src/main/java/com/sbrf/reboot/repository/AccountRepositoryImpl.java
+++ src/main/java/com/sbrf/reboot/repository/AccountRepositoryImpl.java
@@ -3,26 +3,16 @@ package com.sbrf.reboot.repository;
 import com.sbrf.reboot.service.Account;
 import com.sbrf.reboot.service.AccountRepository;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.nashorn.internal.ir.annotations.Immutable;
-import lombok.SneakyThrows;
 
 public class AccountRepositoryImpl implements AccountRepository {
     private String localPath;
-    ObjectMapper mapper = new ObjectMapper();
     private long id;
 
     public AccountRepositoryImpl(String localPath) {
@@ -36,31 +26,33 @@ public class AccountRepositoryImpl implements AccountRepository {
     public Set<Account> getAllAccountsByClientId(long id) throws FileNotFoundException {
         Set<Account> accounts = new HashSet();
         String json = null;
-        try {
-            json = new String(Files.readAllBytes(Paths.get(localPath)));
-        } catch (
-                NoSuchFileException ex) {
-
+        try (BufferedReader reader = new BufferedReader(new FileReader(localPath))) {
+            String line;
+            int localCnt = 0;
+            Long localClientId = null;
+            String localNumber = null;
+            while ((line = reader.readLine()) != null) {
+                if (line.indexOf("\"clientId\": ") > 0) {
+                    localClientId = Long.parseLong(line.substring(line.indexOf("\": ") + 3, line.length() - 1));
+                }
+                if (line.indexOf("\"number\": ") > 0) {
+                    localNumber = line.substring(line.indexOf("\": \"") + 4, line.length() - 1);
+                    localCnt = 1;
+                }
+                if (localCnt == 1) {
+                    System.out.println(localClientId + "  -  " + localNumber);
+                    if (localClientId == id) {
+                        accounts.add(new Account(localClientId, localNumber));
+                    }
+                    localCnt = 0;
+                }
+            }
+        } catch (FileNotFoundException e) {
             throw new FileNotFoundException(localPath);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try {
-            List<Account> listAccount = Arrays.asList(mapper.readValue(json, Account[].class));
-//            for (Account nextAccount : listAccount) {
-//                if (nextAccount.getClientId() == id) {
-//                    accounts. add(nextAccount);
-//                }
-//            }
-// переделал на stream
-            accounts = listAccount.stream().filter(account -> account.getClientId() == id).collect(Collectors.toSet());
-//            System.out.println("Выводим содержимое stream");
-//            System.out.println(accounts);
-
-            return accounts;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        System.out.println(accounts);
+        return accounts;
     }
 }
